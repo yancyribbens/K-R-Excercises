ad9ee1e896433af0d83fab757f754793b212d5c7
yancy <email@yancy.lol>
2023-11-08 09:13:42 +0100

    Add exercise 1-11

---
+++ ex1-11.c
@@ -0,0 +1,12 @@
+/* Exercise 1-11
+ *
+ * How would you test the word count program?  What kind of input
+ * are most likely to uncover bugs if there are any?
+ */
+
+/*
+ * To test the count program, build and compile, then
+ * send words to be counted to stdin.
+ * The most likely to uncover bugs are chars such as space
+ * newline and tab where word separation is created.
+ */
