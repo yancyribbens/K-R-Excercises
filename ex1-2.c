89abb736268167dfea554ed681cf1d5273e0a17f
yancy <email@yancy.lol>
2023-11-08 08:23:16 +0100

    Add excercise 1-2

---
+++ ex1-2.c
@@ -0,0 +1,13 @@
+/* Exercise 1-2
+ *
+ * Experiment to find out what happens when print's
+ * argument string contains \c, where c is some
+ * character not list above.
+ */
+
+#include <stdio.h>
+
+main()
+{
+    printf("hell\o, world\n");
+}
